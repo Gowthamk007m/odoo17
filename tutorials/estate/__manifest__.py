4c8378c462641bcd06ae585d243b313420c744ef
Gowtham K M <gowthamk007m@gmail.com>
2025-07-08 20:22:45 +0530

    Initial

---
+++ tutorials/estate/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Estate',
+    'version': '1.0',
+    'summary': 'Real Estate',
+    'sequence': -100,
+    'description': """
+        Real Estate
+    """,
+    'category': 'Real Estate',
+    
+}
