fab7b96638e9b741bceebc182fad69afecc8c6a0
Gowtham K M <gowthamk007m@gmail.com>
2025-07-10 17:26:58 +0530

    Tree and forms view customaiztion

--- addons/om_hospital/models/patients.py
+++ addons/om_hospital/models/patients.py
@@ -4,6 +4,6 @@ class Patients(Model):
     _name='hospital.patients'
     _description = 'Hospital Patients model'
 
-    name = fields.Char()
+    name = fields.Char(required=True)
     age = fields.Integer()
     gender = fields.Selection([('male', 'Male'), ('female', 'Female')])
