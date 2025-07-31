ef46d948256f47741606b8cb3af8c276036e0154
Gowtham K M <gowthamk007m@gmail.com>
2025-07-31 18:42:01 +0530

    creating statusbar

--- addons/om_hospital/models/appoinments.py
+++ addons/om_hospital/models/appoinments.py
@@ -10,6 +10,7 @@ class HospitalAppointment(models.Model):
     patient_id=fields.Many2one('hospital.patients',string="Patient")
     data_appointment=fields.Date(string="Date")
     note=fields.Text(string="Note")
+    state=fields.Selection([('draft','Draft'),('confirm','Confirm'),('ongoing','Ongoing'),('done','Done'),('cancel','Cancel')],default='draft')
     
     @api.model_create_multi
     def create(self, vals_list):
@@ -17,3 +18,15 @@ class HospitalAppointment(models.Model):
             if not vals.get('reference') or vals['reference'] == 'New':
                 vals['reference'] = self.env['ir.sequence'].next_by_code('hospital.appointment') 
         return super().create(vals_list)
+    
+    def action_confirm(self):
+        self.write({'state':'confirm'})
+        
+    def action_ongoing(self):
+        self.write({'state':'ongoing'})
+        
+    def action_done(self):
+        self.write({'state':'done'})
+        
+    def action_cancel(self):
+        self.write({'state':'cancel'})
--- addons/om_hospital/models/patients.py
+++ addons/om_hospital/models/patients.py
@@ -8,3 +8,4 @@ class Patients(Model):
     name = fields.Char(required=True ,tracking=True)
     age = fields.Integer(tracking=True)
     gender = fields.Selection([('male', 'Male'), ('female', 'Female')],tracking=True)
+    
