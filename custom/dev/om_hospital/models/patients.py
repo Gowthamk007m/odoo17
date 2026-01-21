090076e1c47d8855aee9202f128a7f5d0cb42ef7
Gowtham K M <gowthamk007m@gmail.com>
2026-01-21 18:25:04 +0530

    om_hospital chatters

--- custom/dev/om_hospital/models/patients.py
+++ custom/dev/om_hospital/models/patients.py
@@ -5,8 +5,15 @@ class HospitalPatients(models.Model):
         _inherit=['mail.thread']
         _description = 'Hospital Patients Records'
 
-        name=fields.Char(string="Patient Name",required=True)
-        age=fields.Integer(string="Age",required=True)
-        is_child=fields.Boolean(string="Is Child?",default=False)
+        name=fields.Char(string="Patient Name",required=True,tracking=True)
+        age=fields.Integer(string="Age",required=True,tracking=True)
+        is_child=fields.Boolean(string="Is Child?",default=False,tracking=True)
         notes=fields.Text(string="Notes")
         gender=fields.Selection([('M','Male'),('F','Female')],string="Gender")
+
+        @api.onchange('age')
+        def _onchange_age(self):
+                if self.age<=10:
+                        self.is_child=True
+                else:
+                        self.is_child=False
